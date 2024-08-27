accba62ec201db5758ec44a3bcc52b8ab1257b07
Alex Dowling <adowling@nd.edu>
2024-08-27 08:14:59 -0400

    Updated helper function

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -6,6 +6,21 @@ import requests
 import urllib
 
 import subprocess
+import matplotlib.pyplot as plt
+
+def set_plotting_style():
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 16
+    BIGGER_SIZE = 18
+
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.rc('lines', linewidth=3)
 
 def _check_available(executable_name): return (shutil.which(executable_name) or os.path.isfile(executable_name)) 
 
